347581e0004bf3246252c44add0378e29d6b408a
akathpal <akathpal@umd.edu>
2019-07-18 04:31:19 -0400

    wall_follower

---
+++ wall_follower.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=~/udacity_ws/src/RoboND-HomeServiceRobot/worlds/U-World.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:=~/udacity_ws/src/RoboND-HomeServiceRobot/turtlebot_simulator/turtlebot_gazebo/launch/includes/gmapping.launch.xml " &
+#xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+sleep 3
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 3
+xterm  -e  " rosrun wall_follower wall_follower  "
